786d318634d7888366b8ef40acff01e67f903106
oleg <ojiegoff@gmail.com>
2022-12-01 17:40:30 +0500

    дз2_1-5_4

--- DZ_sem2_4.py
+++ DZ_sem2_4.py
@@ -1,4 +1,6 @@
 import random
+data = open('file.txt', 'w')
+data.close()
 def write_file(number):
     with open('file.txt', 'w') as data:
         for i in range(number):
@@ -7,3 +9,13 @@ def read_file():
     with open('file.txt', 'r') as data:
         indexs = list(map(int,data.readlines()))
     return indexs
+n = int(input("Введите число N: "))
+number = [i for i in range(-n, n+1)]
+write_file(n)
+lst_index = read_file()
+prod = 1
+for i in range(len(lst_index)):
+    
+    prod *= number[lst_index[i]]
+print(f"{number} Результат равен = {prod} ")
+data.close()
