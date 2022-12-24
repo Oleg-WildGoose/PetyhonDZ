8b9b5f65f3d8f155bb4883be5cc94b52ab1bfbe1
oleg <ojiegoff@gmail.com>
2022-12-24 23:44:49 +0500

    дз6_1-2

--- DZ-seminar 6/DZ_sem6_5_enumerate.py
+++ DZ-seminar 6/DZ_sem6_5_enumerate.py
@@ -1,3 +1,15 @@
-names = ["Johnny", "Adam", "Mark"]
-enum_names = list(enumerate(names, start=2))
-print(enum_names)
+# Изменённая задача 4_2 enimarate
+
+num = int(input("Введите число: "))
+i = 2 
+lst = []
+old = num
+while i <= num:
+    if num % i == 0:
+        lst.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+lst = list(enumerate(lst, start=1))
+print(f"Простые множители числа {old} приведены в списке: {lst}")
